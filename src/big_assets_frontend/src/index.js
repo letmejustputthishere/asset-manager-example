8df768d65487d0e24eaa4dd994e5a557019ef20a
Moritz Fuller <moritz.fuller@dfinity.org>
2023-03-01 11:56:11 +0100

    Initial commit.

---
+++ src/big_assets_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { big_assets_backend } from "../../declarations/big_assets_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await big_assets_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
